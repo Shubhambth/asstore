8c85b5e0846ab8e445c85b398bb7671b7418f7cb
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2024-11-20 17:44:48 +0000

    v1.2.8

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='tmp/media/product_image/',default='logo.png')
+    image_url = models.URLField(max_length=200 , default='https://ibb.co/NNxVc3V')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)

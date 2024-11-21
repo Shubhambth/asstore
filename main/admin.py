cb4dfb392a13eff1dac0d5c2055b73f0d4ca4fcb
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2024-11-21 04:29:49 +0000

    v1.3.2

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Product , Purchase , PaymentQr
 
+class PaymentAdmin(admin.ModelAdmin):
+  list_display = ('user', 'product' ,'payment_screenshot', 'mobile_number' ,'email' , 'purchased_at'  )
+
+  @admin.display(description='Image')
+  def image_tag(self, obj):
+      if obj.image:
+          return format_html('<img src="{}" style="height: 100px;"/>', obj.image.url)
+      return 'No Image'
+
 admin.site.register(Product)
-admin.site.register(Purchase)
+admin.site.register(Purchase , PaymentAdmin)
 admin.site.register(PaymentQr)
